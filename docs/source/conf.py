e0b4306f3b4338bd0db1cd0b811e652d666e46e6
Roman Wixinger <roman.wixinger@gmail.com>
2024-01-20 14:11:26 +0100

    [main] Added missing dependencies for ReadTheDocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ copyright = '2023, Di Bartolomeo, Vischi, Grossi, Wixinger'
 author = 'Di Bartolomeo, Vischi, Grossi, Wixinger'
 
 release = '1.0'
-version = '1.0.3'
+version = '1.0.4'
 
 import os
 import sys
