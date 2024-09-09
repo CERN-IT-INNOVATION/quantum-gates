6de6d2bc67a6edf010efe63b0947864e52ebaad2
Roman Wixinger <roman.wixinger@gmail.com>
2024-09-09 18:05:20 +0200

    [library] Added Paolo to the list of contributors.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='quantum-gates',
-    version='1.0.4',
-    author='M. Grossi, G. D. Bartolomeo, M. Vischi, R. Wixinger',
-    author_email='michele.grossi@cern.ch, dibartolomeo.giov@gmail.com, vischimichele@gmail.com, roman.wixinger@gmail.com',
+    version='1.1.0',
+    author='M. Grossi, G. D. Bartolomeo, M. Vischi, P. Da Rold, R. Wixinger',
+    author_email='michele.grossi@cern.ch, dibartolomeo.giov@gmail.com, vischimichele@gmail.com, PAOLO.DAROLD@studenti.units.it, roman.wixinger@gmail.com',
     packages=['quantum_gates',
               'quantum_gates._gates',
               'quantum_gates._legacy',
