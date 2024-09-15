8e088d3fbe608f6c482c136307c23e1e287edb00
Roman Wixinger <roman.wixinger@gmail.com>
2024-09-15 15:56:16 +0200

    [main] Improved handling of the IBM token.

---
+++ configuration/token.py
@@ -0,0 +1,21 @@
+""" Instructions on how to savely handle secrets.
+
+Instructions:
+- Read https://dev.to/emma_donery/python-dotenv-keep-your-secrets-safe-4ocn to understand how python-dotenv works.
+- Create a .env file from the .env.template file.
+- Make sure to never add the .env file to version control. It is also in the .gitignore.
+"""
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Note: Do not hardcode these values.
+IBM_TOKEN = os.environ['IBM_TOKEN']
+HUB = os.environ['HUB']
+GROUP = os.environ['GROUP']
+PROJECT = os.environ['PROJECT']
+DEVICE_NAME = os.environ['DEVICE_NAME']
