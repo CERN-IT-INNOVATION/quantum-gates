e06d8abd2a5f30b3da0802d75c7c4f4bf358d8ef
Roman Wixinger <roman.wixinger@gmail.com>
2024-09-15 16:10:58 +0200

    [main] New release 2.0.0. The recent upgrade to Qiskit 1.2.0 was a major breaking change.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='quantum-gates',
-    version='1.1.0',
+    version='2.0.0',
     author='M. Grossi, G. D. Bartolomeo, M. Vischi, P. Da Rold, R. Wixinger',
     author_email='michele.grossi@cern.ch, dibartolomeo.giov@gmail.com, vischimichele@gmail.com, PAOLO.DAROLD@studenti.units.it, roman.wixinger@gmail.com',
     packages=['quantum_gates',
